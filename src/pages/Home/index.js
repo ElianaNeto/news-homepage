d3988a2be1bb113a01732bae50c40d70864ea56a
Eliana Neto <eliananeto29@gmail.com>
2023-08-05 00:34:12 +0100

    second commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,67 @@
 import './styles.js'
-import { HomeContainer, MainContainer, SecondContainer, MainContainerA, MainContainerB } from './styles.js';
+import mainImag from '../../assets/images/image-web-3-desktop.jpg'
+import {
+  HomeContainer, MainContainer, SecondContainer,
+  MainContainerA, MainContainerB,
+  ContainerBTitle, ContainerBParagraph,
+  NormalTitle, ContentDivB, StyledImage, MainContainerAContent, MainContainerAContentTitle, MainContainerAContentDesc, Button, MainContainerADiv, ImageDiv
+} from './styles.js';
 
-function Home(){
+function Home() {
   return (
     <HomeContainer>
       <MainContainer>
-      <MainContainerA></MainContainerA>
-      <MainContainerB></MainContainerB>
+        <MainContainerA>
+          <ImageDiv>
+            <StyledImage src={mainImag} alt="Imagem" />
+          </ImageDiv>
+
+
+          <MainContainerAContent>
+
+            <MainContainerAContentTitle>The Bright Future of Web 3.0?</MainContainerAContentTitle>
+
+            <MainContainerADiv>
+              <MainContainerAContentDesc>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+                But is it really fulfilling its promise?</MainContainerAContentDesc>
+              <Button>READ MORE</Button>
+
+            </MainContainerADiv>
+
+
+
+          </MainContainerAContent>
+        </MainContainerA>
+        <MainContainerB>
+          <ContainerBTitle>New</ContainerBTitle>
+          <ContainerBParagraph></ContainerBParagraph>
+
+          <ContentDivB hasBorder={true}>
+            <NormalTitle>Hydrogen VS Electric Cars</NormalTitle>
+            <ContainerBParagraph>Will hydrogen-fueled cars ever catch up to EVs?</ContainerBParagraph>
+          </ContentDivB>
+
+          <ContentDivB hasBorder>
+            <NormalTitle>The Downsides of AI Artistry</NormalTitle>
+            <ContainerBParagraph>What are the possible adverse effects of on-demand AI image generation?
+            </ContainerBParagraph>
+          </ContentDivB>
+
+          <ContentDivB>
+            <NormalTitle>Is VC Funding Drying Up?</NormalTitle>
+            <ContainerBParagraph>
+              Private funding by VC firms is down 50% YOY. We take a look at what that means.
+            </ContainerBParagraph>
+          </ContentDivB>
+
+
+
+        </MainContainerB>
       </MainContainer>
-      <SecondContainer/>
+      <SecondContainer />
     </HomeContainer>
   )
-  
+
 
 }
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,40 +1,109 @@
 import styled from "styled-components";
-export const HomeContainer = styled.div `
+export const HomeContainer = styled.div`
   align-items: center;
-background-color: aqua;
-margin: 60px 140px 0 150px;
+margin: 40px 140px 0 150px;
 width: 80%;
 `;
 export const MainContainer = styled.div`
-  //padding-top: 3rem ;
-  //padding-bottom: 3rem ;
-  //align-items: center;
-  //justify-content: center;
   flex-direction:row;
   display: flex;
   box-sizing: border-box;
-background-color: aliceblue;
 height: 510px;
-
-
 `;
 
-export const SecondContainer = styled.div `
-background-color: aquamarine;
+
+export const SecondContainer = styled.div`
 margin-top: 40px;
 height: 150px;
-`;
-
-export const MainContainerA = styled.div `
 background-color: red;
+`;
+
+export const MainContainerA = styled.div`
 height: 100%;
-width: 98% ;
-margin-right:70px;
+width: 90% ;
+margin-right:30px;
+`;
+
+export const ImageDiv = styled.div`
+width: 100%;
+  height: 50%;
+margin-bottom: 25px;
+
+  `;
+
+export const StyledImage = styled.img`
+width: 100%;
+  height: 100%;
+  object-fit: cover; `;
+
+export const MainContainerAContent = styled.div`
+height: 50%;
+  `;
+
+export const MainContainerAContentTitle = styled.p`
+width: 40%;
+height: 50%;
+float: left;
+font-size: 60px;
+margin-top: 10px;
+
+font-weight: bold;
+font-family: 'Inter', sans-serif;
+  `;
+
+export const MainContainerADiv = styled.div`
+  float: right;
+  align-items: center;
+  width: 45%;
+  margin:0;
+  `;
+
+export const MainContainerAContentDesc = styled.p`
+//width: 50%;
+color: hsl(236, 13%, 42%);
+font-family: 'Inter', sans-serif;
+font-size: 15px;
+width: 350px;
+letter-spacing:1px;
+line-height: 25px;
+margin-bottom: 55px;
+`;
+
+export const Button = styled.button`
+  background-color: hsl(5, 85%, 63%);
+  color: white;
+  padding: 15px 30px;
+  border: none;
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 3px;
 `;
 
 
-export const MainContainerB = styled.div `
-background-color: rebeccapurple;
-height: 100%;
+export const MainContainerB = styled.div`
+background-color:  hsl(240, 100%, 5%);
 width: 35% ;
+text-align: start;
+padding: 30px 0 0 30px;
 `;
+
+export const ContainerBTitle = styled.h1`
+color:hsl(35, 77%, 62%);
+`;
+
+export const NormalTitle = styled.h3`
+color:  hsl(36, 100%, 99%);
+`;
+
+export const ContainerBParagraph = styled.p`
+color:  hsl(233, 8%, 79%);
+font-size: 14px;
+`
+export const ContentDivB = styled.div`
+border-bottom: ${props => (props.hasBorder ? 'solid 0.4px hsl(233, 8%, 79%)' : 'none')};
+width: 250px;
+margin-right: 25px;
+padding-bottom: 15px;
+align-items: center;
+
+`
